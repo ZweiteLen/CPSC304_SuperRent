ff9ba805bbc95dc1cf65e1124155daba6e04119d
Glenn <belluminferi492@gmail.com>
2019-11-22 15:58:18 -0800

    worked on receipt

--- src/ca/ubc/cpsc304/database/DatabaseConnectionHandler.java
+++ src/ca/ubc/cpsc304/database/DatabaseConnectionHandler.java
@@ -120,12 +120,6 @@ public class DatabaseConnectionHandler {
      * 4) Update reservation
      */
 
-    /**
-     * Transactions Implementation Start
-     * These are from the terminalTransactions
-     * We can use them as examples
-     */
-
     // Helper function to check if customer already exists in Customer relation.
     private boolean checkCustomerExists(PreparedStatement ps, ReservationModel reservationModel)
 			throws SQLException {
