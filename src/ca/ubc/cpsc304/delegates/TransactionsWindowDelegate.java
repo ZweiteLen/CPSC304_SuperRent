25e99081718ab515fcd11529fb3364f123163b64
Alfred Chang <homefolder@Alfreds-MacBook-Pro.local>
2019-11-23 15:08:42 -0800

    Added interface method to check if a vehicle type exists before customer can reserve such a vehicle

--- src/ca/ubc/cpsc304/delegates/TransactionsWindowDelegate.java
+++ src/ca/ubc/cpsc304/delegates/TransactionsWindowDelegate.java
@@ -32,6 +32,7 @@ public interface TransactionsWindowDelegate {
 //    void insertRentVehicle(RentModel model);
 //    void deleteRentVehicle(String vid);
 //    void updateRentVehicle(String vid, RentModel model);
+    boolean isVehicleAvailable(String vehicleType);
     DefaultTableModel showRentalVehicles(String vtname, String location, String fromDateTime, String toDateTime) throws Exception;
 
     // Just vehicles themselves
